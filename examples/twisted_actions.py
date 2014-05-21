cce1be64f5a76dae5da9cc0e305d05ee9cf78210
rwall <richard@the-moon.net>
2014-05-21 10:03:12 +0100

    Add division for good measure.

--- examples/twisted_actions.py
+++ examples/twisted_actions.py
@@ -2,7 +2,7 @@
 """
 Download a URL and log information about the response.
 """
-from __future__ import print_function, absolute_import
+from __future__ import absolute_import, division, print_function
 import sys
 from pprint import pprint
 
