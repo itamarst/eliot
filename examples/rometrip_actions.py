b9b9017bbf386abcc65d4a8a9d5360ecf13c5ab7
Itamar Turner-Trauring <itamar@itamarst.org>
2015-07-24 17:34:39 -0400

    Simplify further.

--- examples/rometrip_actions.py
+++ examples/rometrip_actions.py
@@ -1,13 +1,6 @@
-from __future__ import unicode_literals
 from sys import stdout
-
 from eliot import start_action, start_task, to_file
 to_file(stdout)
-
-
-class Person(object):
-    def __init__(self, name):
-        self.name = name
 
 
 class Place(object):
@@ -17,21 +10,18 @@ class Place(object):
 
     def visited(self, person):
         with start_action(action_type="place:visited",
-                          person=person.name,
-                          place=self.name):
+                          person=person, place=self.name):
             for thing in self.contained:
                 thing.visited(person)
 
 
 def honeymoon(family):
-    with start_task(action_type="honeymoon",
-                    family=[person.name for person in family]):
-        rome = Place("Rome, Italy", [Place("Vatican Museum",
-                                           [Place("Statue #1"),
-                                            Place("Statue #2")])])
+    with start_task(action_type="honeymoon", family=family):
+        rome = Place("Rome, Italy",
+                     [Place("Vatican Museum",
+                            [Place("Statue #1"), Place("Statue #2")])])
         for person in family:
             rome.visited(person)
 
 
-if __name__ == '__main__':
-    honeymoon([Person("Mrs. Casaubon"), Person("Mr. Casaubon")])
+honeymoon(["Mrs. Casaubon", "Mr. Casaubon"])
