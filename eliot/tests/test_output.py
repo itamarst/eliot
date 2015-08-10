e636fbc687f6fd10e3bb985c8bf9c602eb10f323
Jonathan Lange <jml@mumak.net>
2015-08-10 19:04:19 +0100

    Don't test task_uuid and task_level values

--- eliot/tests/test_output.py
+++ eliot/tests/test_output.py
@@ -556,16 +556,16 @@ class LoggerTests(TestCase):
 
         def remove(key):
             return [message.pop(key) for message in messages[1:]]
-        task_levels = remove(u"task_level")
-        task_uuids = remove(u"task_uuid")
+
+        # Make sure we have task_level & task_uuid in exception messages.
+        remove(u"task_level")
+        remove(u"task_uuid")
         timestamps = remove(u"timestamp")
 
         self.assertEqual(
-            (task_levels[1][-1] == task_levels[0][-1] + 1,
-             task_uuids[0] == task_uuids[1],
-             abs(timestamps[0] + timestamps[1] - 2 * time()) < 1,
+            (abs(timestamps[0] + timestamps[1] - 2 * time()) < 1,
              messages),
-            (True, True, True,
+            (True,
              [message,
               {"message_type": "eliot:destination_failure",
                "message": logger._safeUnicodeDictionary(message),
