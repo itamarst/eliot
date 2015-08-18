ee34a7e3c41cdc5b5aa029cc554e1652f30502d5
Itamar Turner-Trauring <itamar@itamarst.org>
2015-08-18 16:10:58 -0400

    Output start messages together with failed ones.

--- error-extract.py
+++ error-extract.py
@@ -5,7 +5,7 @@ import sys
 from datetime import datetime
 
 
-def pretty_print(message):
+def pretty_print(message, separator=True):
     """
     Print human-readable messages to stdout.
     """
@@ -24,28 +24,45 @@ def pretty_print(message):
             remaining += add_field(remaining, key, value)
 
     level = "/" + "/".join(map(str, message["task_level"]))
-    output = "%s%s %sZ\n%s\n" % (
+    output = "%s%s %sZ\n%s" % (
         message["task_uuid"],
         level,
         datetime.utcfromtimestamp(message["timestamp"]).time().isoformat(),
         remaining,
     )
-    if not sys.stdout.isatty():
-        output = output.encode("utf-8")
+    if separator:
+        output += "\n"
     sys.stdout.write(output)
 
 
 def main():
+    cached_start_messages = {}
+
     for line in sys.stdin:
         try:
             message = json.loads(line)
         except ValueError:
             # Stupid systemd/journald corrupted the log message
             continue
-        if (message.get("message_type") == "eliot:traceback" or
-            (message.get("action_type") and
-             message["action_status"] == "failed")):
+        if message.get("message_type") == "eliot:traceback":
             pretty_print(message)
+        elif message.get("action_type"):
+            action_type = message["action_type"]
+            task_uuid = message["task_uuid"]
+            task_level = tuple(message["task_level"][:-1])
+            status = message["action_status"]
+            key = (action_type, task_uuid, task_level)
+            if status == "started":
+                cached_start_messages[key] = message
+            else:
+                try:
+                    start_message = cached_start_messages.pop(key)
+                except KeyError:
+                    start_message = None
+                if status == "failed":
+                    if start_message:
+                        pretty_print(start_message, separator=False)
+                    pretty_print(message)
 
 
 main()
