6d145c22281a6247650cdc2ae316e0d7801e0530
Itamar Turner-Trauring <itamar@itamarst.org>
2015-10-22 12:02:22 -0400

    Realistically this may be full of random other stuff so give up on enforcement.

--- eliot/_message.py
+++ eliot/_message.py
@@ -155,9 +155,7 @@ class WrittenMessage(PClass):
     @ivar _logged_dict: The originally logged dictionary.
     """
 
-    _logged_dict = pmap_field(
-        (str, unicode),
-        (str, unicode, int, float, list, dict, tuple, type(None)))
+    _logged_dict = pmap_field((str, unicode), object)
 
     @property
     def timestamp(self):
