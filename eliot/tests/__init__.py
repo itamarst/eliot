71d19945bdc0ee3012d2e3ef374398ff03dc2504
Itamar Turner-Trauring <itamar@itamarst.org>
2019-02-10 16:49:00 -0500

    Increase the deadline.

--- eliot/tests/__init__.py
+++ eliot/tests/__init__.py
@@ -1,3 +1,8 @@
 """
 Tests for the eliot package.
 """
+
+# Increase hypothesis deadline so we don't time out on PyPy:
+from hypothesis import settings
+settings.register_profile("eliot", deadline=1000)
+settings.load_profile("eliot")
